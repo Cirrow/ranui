fa671d695a8613c314a1f6db382f1d2a1d81e500
Cieron <cirrow@proton.me>
2026-07-31 09:39:23 +1200

    extra indentation fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         {#each children as child (child.name)}
-        <ChildCard {child} />
+            <ChildCard {child} />
         {/each}
     </div>
 </main>
